90a0102b937d26804de8b945dff949549d7c5050
Alewi Ahmed <alewimail@gmail.com>
2018-04-28 14:06:27 +0300

    Created the layout & put everything in its place.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,36 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return <div className="App">Empty project</div>;
+    return (
+      <div className="App">
+        <div className="container">
+          <div className="outer-circle">
+            <div className="inner-container">
+              <div className="game-control">
+                <div className="row">
+                  <h1>Simon</h1>
+                  <span className="copyright">®</span>
+                </div>
+                <div className="row">
+                  <div>display</div>
+                  <div>start</div>
+                  <div>strict</div>
+                </div>
+                <div>on/off</div>
+              </div>
+              <div className="row">
+                <div className="single-button green" />
+                <div className="single-button red" />
+              </div>
+              <div className="row">
+                <div className="single-button yellow" />
+                <div className="single-button blue" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
